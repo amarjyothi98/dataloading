ed108025e5da14e25e017d1ea9929315464a7d89
amarjyothi98 <amarjyothi526@gmail.com>
2023-06-05 17:58:20 +0530

    added the post array and the tanstack/react-query packages

---
+++ src/pages/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useInfiniteQuery } from '@tanstack/react-query'
+
+export default function page() {
+
+    const posts = [
+        { id: 1, title: 'post 1'}, 
+        { id: 2, title: 'post 2'}, 
+        { id: 3, title: 'post 3'}, 
+        { id: 4, title: 'post 4'}, 
+        { id: 5, title: 'post 5'},
+        { id: 6, title: 'post 6'}
+    ]
+
+    //mock database fetch
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
